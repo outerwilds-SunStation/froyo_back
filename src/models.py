23f6b21a2e293c10faea72f0c96dcf9a08483c2c
ggwp <qwerty61441@gmail.com>
2024-11-02 22:30:38 +0900

    빌드 문제 수정중

--- src/models.py
+++ src/models.py
@@ -11,18 +11,18 @@ class ImageData(BaseModel):
     
 get_nickname = create_model(
     "get_nickname",
-    email = (str, None, Field(description="유저 이메일"))
+    email = (str, None)
 )
 
 set_nickname = create_model(
     "set_nickname",
-    email = (str, None, Field(description="유저 이메일")),
-    nickname = (str, None, Field(description="유저 닉네임"))
+    email = (str, None),
+    nickname = (str, None)
 )
 
 generate_image = create_model(
     "generate_image",
-    email = (str, None, Field(description="유저 이메일")),
-    nickname = (str, None, Field(description="유저 닉네임")),
-    image_data = (str, None, Field(description="Base64 인코딩된 이미지 데이터"))
+    email = (str, None),
+    nickname = (str, None),
+    image_data = (str, None)
 )
